51c5b3e0e88aa9114bede9d3f1c4f7e1d19b9c16
Trolleroof <nikhilprabhu06@gmail.com>
2025-12-16 18:07:40 -0800

    adding background service worker

--- site-tutor-extension/src/background/index.ts
+++ site-tutor-extension/src/background/index.ts
@@ -1,13 +1,16 @@
 // Background service worker
-chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === 'captureScreen') {
         chrome.tabs.captureVisibleTab(
             { format: 'png' },
             (dataUrl) => {
-                sendResponse({ dataUrl });
+                sendResponse({ dataUrl })
             }
-        );
-        return true; // Indicates async response
+        )
+        return true // Indicates async response
     }
-});
 
+    if (request.action === 'getTabId') {
+        sendResponse({ tabId: sender.tab?.id ?? null })
+    }
+})
